194022c6d215f75c8589aa0e5daa6862351b5a08
Mritunjay882 <101097898+Mritunjay882@users.noreply.github.com>
2023-04-02 11:58:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let textinput = document.querySelector("#evaluatedText");
+let countdisplay = document.querySelector("#wordCount");
+textinput.addEventListener("input",()=>{
+  let str = textinput.value.trim();
+  if(str==""){
+    countdisplay.innerText = 0;
+    return;
+  }
+  let count = str.split(" ").length;
+  console.log(str.split(" "));
+  countdisplay.innerText = count;
+});
